da9dcf92d25bb29b509d75e53b593f67d959e297
Piotr <trojanpiotrek@interia.pl>
2025-01-12 18:48:23 +0100

    Finalize TO final project

--- semestr 5/Technologie Obiektowe/To_final_project/observer/observer.py
+++ semestr 5/Technologie Obiektowe/To_final_project/observer/observer.py
@@ -7,6 +7,9 @@ class Observable(IObservable):
     def attach(self, observer):
         self.observers.append(observer)
 
+    def detach(self, observer):
+        self.observers.remove(observer)
+
     def notify_observers(self, data):
         for observer in self.observers:
             observer.update(data)
